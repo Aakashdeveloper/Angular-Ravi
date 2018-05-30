8bd3571a8e81e34569c88696cc0b913dea78de77
AAKASH HANDA <AAKASH HANDA>
2018-05-30 07:27:57 +0530

    A

--- TemplateDrivenForms/src/app/app.component.ts
+++ TemplateDrivenForms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-
+import {Employee} from './models/employee.model'
 
 @Component({
   selector:'app-form',
@@ -7,5 +7,29 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent{
- 
+  languages=["NodeJs","AngularJS","ReactJs"]
+
+  model = new Employee('',"Angular",true,",male","NodeJs")
+
+  hasCodeError= false;
+  firstToUpper(value:string){
+    if(value.length>0){
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1)
+    }else{
+      this.model.firstName = value
+    }
   }
+
+  validateCodeLang(event){
+    if(this.model.codelang === "default")
+      this.hasCodeError=true
+    else
+      this.hasCodeError=false
+  }
+  }
+
+  /*
+  ng-pristine ng-untouched ng-invalid
+  ng-dirty    ng-touched   ng-valid
+  */
+
